20cc1d7def2d24b32a884925ee224d4a33ff1230
Aashlesha <chitteashlesha@gmail.com>
2023-07-10 18:58:58 +0530

    Google SEO

---
+++ src/Login.spec.js
@@ -0,0 +1,7 @@
+import * as React from 'react';
+import { mount } from 'enzyme';
+describe('login', () =>{
+    it('positive test case', () => {
+
+    })
+});
---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from 'react-dom';
+import "./style.css";
+import Login from "./Login";
+
+ReactDOM.render(
+  <div className="g-signin">
+      <Login />
+  </div>,
+  document.getElementById('root'),
+);
+
